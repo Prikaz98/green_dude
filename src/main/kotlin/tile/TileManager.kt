355202de258a122d5060f57f9514a99e00b257f4
IvanPrikaznov <prikaznov555@mail.ru>
2022-12-20 18:32:41 +0300

    up

--- src/main/kotlin/tile/TileManager.kt
+++ src/main/kotlin/tile/TileManager.kt
@@ -2,6 +2,8 @@ package tile
 
 import panels.GamePanel
 import java.awt.Graphics2D
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import javax.imageio.ImageIO
 
 class TileManager(gp: GamePanel) {
@@ -14,13 +16,28 @@ class TileManager(gp: GamePanel) {
 
     private fun getTitleImage() {
         tiles.set(0, Tile(false, ImageIO.read(javaClass.getResourceAsStream("/texture/sand.jpg"))))
-        tiles.set(1, Tile(false, ImageIO.read(javaClass.getResourceAsStream("/texture/water.png"))))
-        tiles.set(2, Tile(true, ImageIO.read(javaClass.getResourceAsStream("/texture/wall.png"))))
+        tiles.set(1, Tile(true, ImageIO.read(javaClass.getResourceAsStream("/texture/wall.png"))))
+        tiles.set(2, Tile(false, ImageIO.read(javaClass.getResourceAsStream("/texture/water.png"))))
     }
 
     fun draw(g2: Graphics2D) {
-        g2.drawImage(tiles.get(0)?.image, 0, 0, tileSize, tileSize, null)
+        val map = BufferedReader(InputStreamReader(javaClass.getResourceAsStream("/map")))
+
+        data class ElementChar(val x: Int, val selectTile: Int)
+        data class ElementRow(val y: Int, val cells: List<ElementChar>)
+        val mapData = map.lineSequence().foldIndexed(ArrayList<ElementRow>()) { index, acc, row ->
+            val chars = row.toCharArray().asSequence().foldIndexed(ArrayList<ElementChar>(), { index, accChar, selectTile ->
+                accChar.add(ElementChar(index, selectTile.digitToInt()))
+                accChar
+            })
+            acc.add(ElementRow(index, chars))
+            acc
+        }
+        mapData.forEach{ elementRow ->
+            elementRow.cells.forEach{ cell ->
+                g2.drawImage(tiles.get(cell.selectTile)?.image, cell.x * tileSize, elementRow.y * tileSize, tileSize, tileSize, null)
+            }
+        }
+
     }
-
-
 }
